946220d89da8ebb3f4b15d1af0719caed03beb3b
andriawan24 <fawaznaufal23@gmail.com>
2020-04-14 14:20:03 +0700

    Initial COmmit

--- examples/endMeeting.php
+++ examples/endMeeting.php
@@ -1,19 +1,8 @@
 <?php
 
-/* _____ PHP Big Blue Button API Usage ______
-* by Peter Mentzer peter@petermentzerdesign.com
-* Use, modify and distribute however you like.
-*/
-
-// Require the bbb-api file:
 require_once('../includes/bbb-api.php');
 
-// Instatiate the BBB class:
 $bbb = new BigBlueButton();
-
-/* ___________ END A MEETING ______ */
-/* Determine the meeting to end via meetingId and end it.
-*/
 
 $endParams = array(
 	'meetingId' => '1234', 			// REQUIRED - We have to know which meeting to end.
@@ -23,28 +12,22 @@ $endParams = array(
 
 // Get the URL to end a meeting:
 $itsAllGood = true;
-try {$result = $bbb->endMeetingWithXmlResponseArray($endParams);}
-	catch (Exception $e) {
-		echo 'Caught exception: ', $e->getMessage(), "\n";
-		$itsAllGood = false;
-	}
+try {
+	$result = $bbb->endMeetingWithXmlResponseArray($endParams);
+} catch (Exception $e) {
+	echo 'Caught exception: ', $e->getMessage(), "\n";
+	$itsAllGood = false;
+}
 
-	if ($itsAllGood == true) {
-		// If it's all good, then we've interfaced with our BBB php api OK:
-		if ($result == null) {
-			// If we get a null response, then we're not getting any XML back from BBB.
-			echo "Failed to get any response. Maybe we can't contact the BBB server.";
-		}	
-		else { 
-		// We got an XML response, so let's see what it says:
+if ($itsAllGood == true) {
+	if ($result == null) {
+		echo "Failed to get any response. Maybe we can't contact the BBB server.";
+	} else {
 		print_r($result);
-			if ($result['returncode'] == 'SUCCESS') {
-				// Then do stuff ...
-				echo "<p>Meeting succesfullly ended.</p>";
-			}
-			else {
-				echo "<p>Failed to end meeting.</p>";
-			}
+		if ($result['returncode'] == 'SUCCESS') {
+			echo "<p>Meeting succesfullly ended.</p>";
+		} else {
+			echo "<p>Failed to end meeting.</p>";
 		}
-	}	
-?>
+	}
+}
--- examples/getJoinMeetingUrlModeratorRecord.php
+++ examples/getJoinMeetingUrlModeratorRecord.php
@@ -1,20 +1,9 @@
 <?php
 
-/* _____ PHP Big Blue Button API Usage ______
-* by Peter Mentzer peter@petermentzerdesign.com
-* Use, modify and distribute however you like.
-*/
-
-// Require the bbb-api file:
 require_once('../includes/bbb-api.php');
 
 
-// Instatiate the BBB class:
 $bbb = new BigBlueButton();
-
-/* ___________ JOIN MEETING w/ OPTIONS ______ */
-/* Determine the meeting to join via meetingId and join it.
-*/
 
 $joinParams = array(
 	'meetingId' => '12345', 			// REQUIRED - We have to know which meeting to join.
@@ -25,16 +14,14 @@ $joinParams = array(
 	'webVoiceConf' => ''				// OPTIONAL - string
 );
 
-// Get the URL to join meeting:
 $itsAllGood = true;
-try {$result = $bbb->getJoinMeetingURL($joinParams);}
-	catch (Exception $e) {
-		echo 'Caught exception: ', $e->getMessage(), "\n";
-		$itsAllGood = false;
-	}
+try {
+	$result = $bbb->getJoinMeetingURL($joinParams);
+} catch (Exception $e) {
+	echo 'Caught exception: ', $e->getMessage(), "\n";
+	$itsAllGood = false;
+}
 
 if ($itsAllGood == true) {
-	//Output results to see what we're getting:
 	print_r($result);
-}	
-?>
+}
